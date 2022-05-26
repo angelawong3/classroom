e3623c02a85d739b4348910982db07c519d7f709
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-26 19:54:49 +0100

    exercise for week 9

---
+++ Week-9/pre-cls-ex/script.js
@@ -0,0 +1,33 @@
+// Ex:
+// Input: 1
+// Output: 'Monday'
+
+// Input: 5
+// Output: 'Friday'
+
+// Input: 8
+// Output: undefined
+
+function nameOfWeekDay(day) {
+  if (day === 1) {
+    return "Monday";
+  }
+  if (day === 2) {
+    return "Tuesday";
+  }
+  if (day === 3) {
+    return "Wednesday";
+  }
+  if (day === 4) {
+    return "Thursday";
+  }
+  if (day === 5) {
+    return "Friday";
+  }
+  if (day === 6) {
+    return "Saturday";
+  }
+  if (day === 7) {
+    return "Sunday";
+  }
+}
