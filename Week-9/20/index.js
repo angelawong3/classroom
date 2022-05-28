e93b7f9e2c36e845edb74ea68ea9c0241de9daa2
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-28 13:50:50 +0100

    exercise 20

---
+++ Week-9/20/index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What languages do you know?",
+    name: "language",
+  },
+  {
+    type: "input",
+    message: "What is your preferred method of communication?",
+    name: " method",
+  },
+]);
