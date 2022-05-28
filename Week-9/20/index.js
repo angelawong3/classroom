365072f66f001f950c7cea0d3455f8686b121286
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-28 14:04:48 +0100

    exercise 20

--- Week-9/20/index.js
+++ Week-9/20/index.js
@@ -1,19 +1,28 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What languages do you know?",
-    name: "language",
-  },
-  {
-    type: "input",
-    message: "What is your preferred method of communication?",
-    name: " method",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What languages do you know?",
+      name: "language",
+    },
+    {
+      type: "input",
+      message: "What is your preferred method of communication?",
+      name: " method",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split("").join("")}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
